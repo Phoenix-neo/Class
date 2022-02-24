eaf2afb6574dcbe1c1f509ac76abd6020c65712b
Radin Hatami <94353465+Phoenix-neo@users.noreply.github.com>
2022-02-24 15:36:48 +0330

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 struct Vector {
 	int size = 0;
+	int maxsize = 1;
 	int* a;
 	Vector() {
 	    a = (int*) malloc(1*sizeof(int));
@@ -11,6 +12,22 @@ struct Vector {
 	void expand() {
 	    /// a ra 2 barar kon
 	    /// copy
+	    int* b = (int*) malloc(maxsize * 2 * sizeof(int));
+	    for (int i = 0; i < size; i++)
+		    b[i] = a[i];
+	    delete(a);
+	    a = b;
+	    maxsize *= 2;
+	}
+
+
+	void deduct() {
+		int* b = (int*) malloc(maxsize / 2 * sizeof(int));
+		for (int i = 0; i < size; i++)
+			b[i] = a[i];
+		delete(a);
+		maxsize /= 2;
+		a = b;
 	}
 
 	int find(int x, int * end) {
@@ -28,16 +45,31 @@ struct Vector {
 
 	void push_back(int x) {
 		int * last = this->end();
-		this->expand();
+		if (size == maxsize)
+			this->expand();
 		int * i = last + 1;
 		* i = x;
 	}
 
 	void pop_back() {
+		if (!this->empty()) {
+			size--;
+			if (size == maxsize / 2) 
+				this->deduct();
+		}
 	}
-
+	bool empty() {
+		if (this->begin() == this->end())
+			return true;
+		else 
+			return false;
+	}
+	int getsize() {
+		return this->end() - this->begin() + 1;
+	}
 	int back() {
-	    return -1;
+		if (!this->empty())
+			return (a[0]);
 	}
 	int * begin() {
 		return a;
@@ -50,3 +82,4 @@ struct Vector {
 int main() {
 	Vector v;
 }
+
