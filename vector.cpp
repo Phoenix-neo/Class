1b7a440b81424d795706f8a3b9fce26c10a1b454
Phoenix-neo <hatamiradin@gmail.com>
2022-02-26 19:58:09 +0330

    Stack but has some bugs to be solved

--- vector.cpp
+++ vector.cpp
@@ -6,79 +6,81 @@ struct Vector {
 	int maxsize = 1;
 	int* a;
 	Vector() {
-	    a = (int*) malloc(1*sizeof(int));
+	    a = (int*) malloc(1 * sizeof(int));
 	}
 
 	void expand() {
-	    /// a ra 2 barar kon
-	    /// copy
 	    int* b = (int*) malloc(maxsize * 2 * sizeof(int));
 	    for (int i = 0; i < size; i++)
 		    b[i] = a[i];
-	    delete(a);
+	    free(a);
 	    a = b;
 	    maxsize *= 2;
 	}
 
 
 	void deduct() {
-		int* b = (int*) malloc(maxsize / 2 * sizeof(int));
+		int* b = (int*) malloc((maxsize / 2) * sizeof(int));
 		for (int i = 0; i < size; i++)
 			b[i] = a[i];
-		delete(a);
+		free(a);
 		maxsize /= 2;
 		a = b;
 	}
 
-	int find(int x, int * end) {
-		int cnt = 0;
-		for (auto i = this->begin(); a != this->end(); i++) {
-			if (*i == x)
-				return cnt;
-			else
-				cnt++;
-		}
+	int find(int x) {
+		for (int i = 0; i < size; i++)
+			if (a[i] == x)
+				return i;
+		return -1;
 	}
 	int get(int index) {
-		return *(this->begin() + index);
+		if (!this->empty() && index < size)
+			return a[index];
+		else 
+			return -1;
 	}
 
 	void push_back(int x) {
-		int * last = this->end();
-		if (size == maxsize)
-			this->expand();
-		int * i = last + 1;
-		* i = x;
+		if (size == maxsize - 1)
+			expand();
+		a[size] = x;
+		size++;
 	}
 
 	void pop_back() {
 		if (!this->empty()) {
 			size--;
-			if (size == maxsize / 2) 
-				this->deduct();
+			if (size == (maxsize / 2) - 1) 
+				deduct();
 		}
 	}
 	bool empty() {
-		if (this->begin() == this->end())
+		if (size == 0)
 			return true;
 		else 
 			return false;
 	}
 	int getsize() {
-		return this->end() - this->begin() + 1;
+		return size;
 	}
 	int back() {
 		if (!this->empty())
 			return (a[0]);
 	}
+	int front() {
+		if (!this->empty())
+			return a[size - 1];
+	}
 	int * begin() {
 		return a;
 	}
 	int * end() {
-		return a + size;
+		return a + size - 1;
 	}
 };
 
 int main() {
 	Vector v;
+	return 0;
 }
